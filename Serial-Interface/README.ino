6305d5d9a5b7faa4b6b10b5b7ec35a5fd5e66885
Michel <mplatnic@users.noreply.github.com>
2018-11-03 14:52:18 +0200

    Update README.ino

--- Serial-Interface/README.ino
+++ Serial-Interface/README.ino
@@ -1,10 +1,13 @@
 The aim of the class is to create a robot controlled by the serial interface.
 
 Let's first understand how the serial interface works with BuzzerTest.ino
-Then instead of sound, let's make the mBot moves with MbotSerialControl.ino
+  then try the SerialReceive.ino
+Finally instead of sound, let's make the mBot moves with MbotSerialControl.ino
 
 Finally, let's control the Mbot with the keyboard so that it can move around
 and do "special moves"
+  
+  You may use MbotSerialControl2.ino as an example.
 
 Then use the 2 sensors, the ultrasonic sensor (with the example: UltrasonicSensorTest)
 in order to 
