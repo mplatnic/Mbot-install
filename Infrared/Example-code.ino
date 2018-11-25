b2fba0cb2ae2cd27a15d1867840f9702f3bbefca
Michel <mplatnic@users.noreply.github.com>
2018-11-25 21:40:50 +0200

    Update and rename Infrared/mBot-Infrared-basic/Example-code.ino to Infrared/Example-code.ino

--- Infrared/Example-code.ino
+++ Infrared/Example-code.ino
@@ -16,30 +16,34 @@ void setup(){
     Serial.begin(9600);
     Serial.println("Infrared Receiver Start!");
     ir.begin();
+    rgb.setNumber(16);
+    rgb.clear();
 }
 
 void loop(){
     if(ir.keyPressed(22)){     //"0" pressed on the remote control
         buzzer.tone(523, 10);
+        rgb.setColor(10, 0, 0);
         move0();
     }
     
     if(ir.keyPressed(12)){     //"1" pressed on the remote control
         buzzer.tone(220, 10);
         move1();
+        rgb.setColor(100, 0, 0);
     }
     ir.loop();
 }
 
 
 void move0(){
-  ...
-  ...
-  ...
+  //...
+  //...
+  //...
 }
 
 void move0(){
-  ...
-  ...
-  ...
+  //...
+  //...
+  //...
 }
