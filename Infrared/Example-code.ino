4b1a4d3d3947657f155d518dd579f19a6a983d1e
Michel <mplatnic@users.noreply.github.com>
2018-11-25 22:02:56 +0200

    Update Example-code.ino

--- Infrared/Example-code.ino
+++ Infrared/Example-code.ino
@@ -2,8 +2,6 @@
 // it is based on the default program that comes with the mBot
 // this shows the format expected
 
-
-#include <Arduino.h>
 #include <Wire.h>
 #include <SoftwareSerial.h>
 
@@ -16,21 +14,17 @@ void setup(){
     Serial.begin(9600);
     Serial.println("Infrared Receiver Start!");
     ir.begin();
-    rgb.setNumber(16);
-    rgb.clear();
 }
 
 void loop(){
     if(ir.keyPressed(22)){     //"0" pressed on the remote control
         buzzer.tone(523, 10);
-        rgb.setColor(10, 0, 0);
         move0();
     }
     
     if(ir.keyPressed(12)){     //"1" pressed on the remote control
         buzzer.tone(220, 10);
         move1();
-        rgb.setColor(100, 0, 0);
     }
     ir.loop();
 }
@@ -42,7 +36,7 @@ void move0(){
   //...
 }
 
-void move0(){
+void move1(){
   //...
   //...
   //...
