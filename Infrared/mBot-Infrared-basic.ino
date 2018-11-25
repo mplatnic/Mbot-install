70e025648c1ef9bacfe06f13fe5510f3414457c3
Michel <mplatnic@users.noreply.github.com>
2018-11-25 21:38:36 +0200

    Rename Infrared/mBot-Infrared-basic/mBot-Infrared-basic.ino to Infrared/mBot-Infrared-basic.ino

---
+++ Infrared/mBot-Infrared-basic.ino
@@ -0,0 +1,46 @@
+//This is an example that uses all the keys of the remote control 
+// it is based on the default program that comes with the mBot
+
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+#include <MeMCore.h>
+
+MeBuzzer buzzer;
+MeIR ir;
+
+void setup(){
+    Serial.begin(9600);
+    Serial.println("Infrared Receiver Start!");
+    ir.begin();
+}
+
+void loop(){
+    if(ir.keyPressed(22)){     //"0" pressed on the remote control
+        buzzer.tone(523, 10);
+    }
+    
+    if(ir.keyPressed(12)){     //"1" pressed on the remote control
+        buzzer.tone(220, 10);
+    }
+    
+    if(ir.keyPressed(24)){     //"2" pressed on the remote control
+        buzzer.tone(247, 10);
+    }
+    
+    if(ir.keyPressed(94)){     //"3" pressed on the remote control
+        buzzer.tone(262, 10);
+    }
+    
+    if(ir.keyPressed(8)){     //"4" pressed on the remote control
+        buzzer.tone(294, 10);
+    }
+    
+    if(ir.keyPressed(28)){     //"5" pressed on the remote control
+        buzzer.tone(330, 10);
+    }
+    ir.loop();
+}
+
